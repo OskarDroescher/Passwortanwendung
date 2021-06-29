8c9627db27128336c0eddfa6a4115b236a48947a
OskarDroescher <86292259+OskarDroescher@users.noreply.github.com>
2021-06-29 09:27:12 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent }, // http://localhost:4200/login/
+  { path: 'dashboard', component: DashboardComponent }, // http://localhost:4200/dashboard/
+  { path: '', redirectTo: 'login', pathMatch: 'full' } // http://localhost:4200/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
